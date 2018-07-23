80d353bb4eec9a8551452ee8c6a952dc7ebe0d23
Marijn Kok <mshkok@gmail.com>
2018-07-23 22:48:22 +0200

    Bio afgemaakt, email knop gefixt en css verbeterd

--- index.php
+++ index.php
@@ -12,24 +12,22 @@ printHeader() ?>
 					<article id="one">
 						<h3>Ik</h3>
 						<p>
-							<img class="photo" src="images/marijn.jpg"/>Hoi! Ik ben Marijn, student Medisch Beeldvormende en Radiotherapeutische Technieken aan de Hanzehogeschool in Groningen. Ik ben groot fan van techniek, waaronder mechanische toetsenborden, de Raspberry Pi en Android. Verder werk ik graag aan creatieve dingen, digitaal of niet. Ik doe 
-							
-							aak gebruik van Cinema 4D om 3D-computergraphics te ontwerpen. Een paar van mijn ontwerpen kun je vinden in de <a href="galerij.php">galerij</a>. Verder pas ik vaak mijn apparaten waar ik mee werk exact aan naar mijn smaak, zodat ik ze optimaal kan gebruiken. Deze site is zelf gemaakt.
+							<img class="photo" src="images/marijn.jpg"/>Hoi! Ik ben Marijn, student Medisch Beeldvormende en Radiotherapeutische Technieken aan de Hanzehogeschool in Groningen. Ik ben groot fan van techniek, waaronder mechanische toetsenborden, de Raspberry Pi en Android. Verder ben ik graag creatief bezig. Ik gebruik mijn telefoon voor fotografie en Cinema&nbsp;4D om 3D-computergraphics te ontwerpen. Mijn werk kun je vinden in de <a href="galerij.php">galerij</a>. Deze site is zelf gemaakt.
 						</p>
 					</article>
 					<article id="two">
 						<h3>Anders nog iets?</h3>
 						<p class="joke">Wat links... ook al staan ze rechts</p>
 						 <ul>
-							<li><a href="galerij.php"><i class="fas fa-image"></i> Galerij</a></li>
-							<li><a href="https://rutgerkok.nl/minecraft/"><i class="fas fa-cube"></i> Wereldbouw</a></li>
-							<li><a href="copyright.php"><i class="fas fa-copyright"></i> Copyright en credits</a></li>
+							<li><a href="galerij.php"><i class="fas fa-image"></i>Galerij</a></li>
+							<li><a href="https://rutgerkok.nl/minecraft/"><i class="fas fa-cube"></i>Wereldbouw</a></li>
+							<li><a href="copyright.php"><i class="fas fa-copyright"></i>Copyright en credits</a></li>
 						</ul> 
 					</article>
 				</div>
 			</main>
 			<footer>
-				<p>Je kan me bereiken via e-mail. <a href="mailto:marijn@marijnk.nl" class="button"><i class="fas fa-paper-plane"></i> Groetjes!</a></p>
+				<p>Je kan me bereiken via e-mail. <a href="mailto:marijn@marijnk.nl" class="button"><i class="fas fa-paper-plane"></i>marijn@marijnk.nl</a></p>
 			</footer>
 		</div>
 	<script src="scroll.js"></script>
