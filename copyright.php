7cbac4d4d0ad2df77b27f246da331c01d8ebb34c
Marijn Kok <mshkok@gmail.com>
2018-09-09 02:51:14 +0200

    Removed spaces, renamed images folder, slightly changed bio (WIP), added lego movies and thumbnails, added retro camera to css and landing page

--- copyright.php
+++ copyright.php
@@ -2,7 +2,7 @@
 require "template.php";
 printCompleteHeader() ?>
 		
-<h2><a class="iconlink" href="<?= WEB_ROOT ?>"><i class="fas fa-caret-left"></a></i> Copyright & credits</h2>
+<h2><a class="iconlink" href="<?= WEB_ROOT ?>"><i class="fas fa-caret-left"></a></i>Copyright & credits</h2>
 <p>© 2018 Marijn Kok</p>
 <p>Achtergrondfoto door <a target="_blank" href="https://unsplash.com/photos/HvYbkXHBBd4">Jorge Barahona</a> op <a target="_blank" href="https://unsplash.com/">Unsplash</a></p>
 <?php printCompleteFooter() ?>
