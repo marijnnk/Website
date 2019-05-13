774c331040fb707df0bee504bbd49805c822e026
Marijn <marijn@marijnk.nl>
2019-05-14 00:58:57 +0200

    Synced some forgotten files and updated LICENSE
    It's already 2019?

--- copyright.php
+++ copyright.php
@@ -1,8 +1,8 @@
-<?php
+﻿<?php
 require "template.php";
 printCompleteHeader() ?>
 		
-<h2><a class="iconlink" href="<?= WEB_ROOT ?>"><i class="fas fa-caret-left"></a></i>Copyright & credits</h2>
-<p>© 2018 Marijn Kok</p>
+<h2><a class="iconlink" href="<?= WEB_ROOT ?>"><i class="fas fa-caret-left"></a></i> Copyright & credits</h2>
+<p>© 2018-2019 Marijn Kok</p>
 <p>Achtergrondfoto door <a target="_blank" href="https://unsplash.com/photos/HvYbkXHBBd4">Jorge Barahona</a> op <a target="_blank" href="https://unsplash.com/">Unsplash</a></p>
 <?php printCompleteFooter() ?>
--- galerij.php
+++ galerij.php
@@ -1,12 +1,22 @@
 <?php
 require "template.php";
 printCompleteHeader() ?>
-		
-<h2><a class="iconlink" href="<?= WEB_ROOT ?>"><i class="fas fa-caret-left"></a></i>Galerij</h2>
 
-<div class="img-wrapper"><a href="media/gallery/nexus4.jpg"><div style="background-image: url(media/gallery/thumbnails/nexus4-thumbnail.jpg)"></div></a></div><div class="img-wrapper"><a href="media/gallery/planck.jpg"><div style="background-image: url(media/gallery/thumbnails/planck-thumbnail.jpg)"></div></a></div>
-<div class="img-wrapper"><a href="media/gallery/nokia.jpg"><div style="background-image: url(media/gallery/thumbnails/nokia-thumbnail.jpg)"></div></a></div><div class="img-wrapper"><a href="media/gallery/science.jpg"><div class="img-bottom" style="background-image: url(media/gallery/thumbnails/science-thumbnail.jpg)"></div></a></div>
-<div class="img-wrapper"><a href="media/gallery/thee.jpg"><div style="background-image: url(media/gallery/thumbnails/thee-thumbnail.jpg)"></div></a></div><div class="img-wrapper"><a href="media/gallery/RGBRun.jpg"><div style="background-image: url(media/gallery/thumbnails/RGBRun-thumbnail.jpg)"></div></a></div>
-<div class="img-wrapper"><a href="media/gallery/kelpboard.jpg"><div style="background-image: url(media/gallery/thumbnails/kelpboard-thumbnail.jpg)"></div></a></div><div class="img-wrapper"><a href="media/gallery/bloem.jpg"><div style="background-image: url(media/gallery/thumbnails/bloem-thumbnail.jpg)"></div></a></div>
-<div class="img-wrapper"><a href="media/gallery/wonderland.jpg"><div style="background-image: url(media/gallery/thumbnails/wonderland-thumbnail.jpg)"></div></a></div><div class="img-wrapper"><a href="media/gallery/macropi.jpg"><div class="img-bottom" style="background-image: url(media/gallery/thumbnails/macropi-thumbnail.jpg)"></div></a></div>
+<h2><a class="iconlink" href="<?= WEB_ROOT ?>"><i class="fas fa-caret-left"></a></i> Gallerij</h2>
+
+<div class="img-wrapper"><a href="images/gallery/nexus4.jpg">
+        <div style="background-image: url(images/gallery/thumbnails/nexus4-thumbnail.jpg)"></div>
+    </a></div><div class="img-wrapper"><a href="images/gallery/planck.jpg">
+        <div style="background-image: url(images/gallery/thumbnails/planck-thumbnail.jpg)"></div>
+    </a></div>
+<div class="img-wrapper"><a href="images/gallery/nokia.jpg">
+        <div style="background-image: url(images/gallery/thumbnails/nokia-thumbnail.jpg)"></div>
+    </a></div><div class="img-wrapper"><a href="images/gallery/RGBRun.jpg">
+        <div style="background-image: url(images/gallery/thumbnails/RGBRun-thumbnail.jpg)"></div>
+    </a></div>
+<div class="img-wrapper"><a href="images/gallery/wonderland.jpg">
+        <div style="background-image: url(images/gallery/thumbnails/wonderland-thumbnail.jpg)"></div>
+    </a></div><div class="img-wrapper"><a href="images/gallery/macropi.jpg">
+        <div class="img-bottom" style="background-image: url(images/gallery/thumbnails/macropi-thumbnail.jpg)"></div>
+    </a></div>
 <?php printCompleteFooter() ?>
