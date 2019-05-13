89137128e2491ca3619d81efb16dcb61040cd5a2
Marijn <marijn@marijnk.nl>
2019-05-14 00:16:42 +0200

    Removed some images, edited some texts, added some css comments
    Long overdue github sync :)

--- index.php
+++ index.php
@@ -12,15 +12,14 @@ printHeader() ?>
 					<article id="one">
 						<h3>Ik</h3>
 						<p>
-							<img class="photo" src="media/marijn.jpg"/>Hoi! Ik ben Marijn, student Medisch Beeldvormende en Radiotherapeutische Technieken aan de Hanzehogeschool in Groningen. Ik ben fan van techniek, waaronder mechanische toetsenborden, de Raspberry Pi en Android. Verder ben ik graag creatief bezig. Ik gebruik mijn telefoon voor fotografie en ik gebruik Cinema&nbsp;4D om 3D-computergraphics te ontwerpen. Mijn werk kun je vinden in de <a href="galerij.php">galerij</a>. Deze site is zelf gemaakt.
+							<img class="photo" src="images/marijn.jpg"/>Hoi! Ik ben Marijn, aankomend student HBO-ICT aan de Hanzehogeschool in Groningen. Ik ben groot fan van techniek, waaronder mechanische toetsenborden, Android en de Raspberry Pi. Verder ben ik graag creatief bezig. Ik gebruik mijn telefoon voor fotografie en voorheen Cinema&nbsp;4D om CGI te ontwerpen. Mijn werk kun je vinden in de <a href="galerij.php">galerij</a>. Deze site is zelf gemaakt.
 						</p>
 					</article>
 					<article id="two">
 						<h3>Anders nog iets?</h3>
 						<p class="joke">Wat links... ook al staan ze rechts</p>
-						 <ul>
+						 <ul class="no-ul-style">
 							<li><a href="galerij.php"><i class="fas fa-image"></i>Galerij</a></li>
-							<li><a href="nostalgie.php"><i class="fas fa-camera-retro"></i>Nostalgie</a>
 							<li><a href="https://rutgerkok.nl/minecraft/"><i class="fas fa-cube"></i>Wereldbouw</a></li>
 							<li><a href="copyright.php"><i class="fas fa-copyright"></i>Copyright en credits</a></li>
 						</ul> 
